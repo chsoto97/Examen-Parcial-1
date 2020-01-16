b72614f93638f9cbfadbf00d1a0d946fbda807c4
Jesús Soto <jssotomdz@gmail.com>
2020-01-16 08:14:13 -0600

    Parte 3

---
+++ Parte-3/index.js
@@ -0,0 +1,22 @@
+function watchButton(){
+	let smtbtn=document.getElementById("submit");
+	smtbtn.addEventListener("click", function (event){
+		event.preventDefault();
+		let name=document.getElementsByClassName("userName");
+		let comment=document.getElementById("userComment");
+		dispName = document.createElement("label");
+		dispName.textContent = name[0].value;
+		dispName.style.fontWeight = "bold";
+		dispComment = document.createElement("label");
+		dispComment.textContent = comment.value;
+		let sectionC=document.getElementsByClassName("seccionComments");
+		sectionC[0].append(dispName);
+		sectionC[0].append(dispComment);
+	});
+}
+
+function init(){
+	watchButton();
+}
+
+init();
